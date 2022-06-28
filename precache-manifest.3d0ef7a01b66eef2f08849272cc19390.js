6801db1685c5186e302a8d72ee0441f2de817c67
Mike <mdlam88@gmail.com>
2022-06-28 16:16:15 -0700

    Updates

---
+++ precache-manifest.3d0ef7a01b66eef2f08849272cc19390.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "88df390c71cc2d3fe13b",
+    "url": "/robofriends/static/css/main.6825651e.chunk.css"
+  },
+  {
+    "revision": "88df390c71cc2d3fe13b",
+    "url": "/robofriends/static/js/main.88df390c.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "a9a53d5fa7b3061464ca",
+    "url": "/robofriends/static/css/2.1fae995c.chunk.css"
+  },
+  {
+    "revision": "a9a53d5fa7b3061464ca",
+    "url": "/robofriends/static/js/2.a9a53d5f.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fc87d3f684e3d6012c84e89300a6a74f",
+    "url": "/robofriends/index.html"
+  }
+];
